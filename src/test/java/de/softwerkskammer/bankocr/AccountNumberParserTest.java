438a19f9e6c12c5f166644b5362d26205ca4d385
Jan Mewes <jan.mewes@experimental-software.com>
2016-05-18 22:24:28 +0200

    [Red] Write a failing test

--- src/test/java/de/softwerkskammer/bankocr/AccountNumberParserTest.java
+++ src/test/java/de/softwerkskammer/bankocr/AccountNumberParserTest.java
@@ -1,27 +1,30 @@
 package de.softwerkskammer.bankocr;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 
 public class AccountNumberParserTest {
-    
+
     @Test
     public void testParseAccountNumber() {
-        
+
         // Each entry is 4 lines long
         // each line 27 characters
         // each account number 9 digits
     }
-    
+
     @Test
     public void testParseDigit() {
-        
+
         char[][] testDigit = new char[][] {
             {' ','_',' '},
             {'|',' ','|'},
             {'|','_','|'},
         };
-        
+
         char digit = AccountNumberParser.parseDigit(testDigit);
+        assertEquals("Cannot parse digit", '0', digit);
     }
 }

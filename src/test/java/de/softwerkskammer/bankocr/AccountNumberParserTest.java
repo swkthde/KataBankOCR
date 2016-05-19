62e4be883fffe74a9e0f7f1c1ffd5830a2f2baef
Jan Mewes <jan.mewes@experimental-software.com>
2016-05-19 08:08:57 +0200

    [Red] Write a failing test

--- src/test/java/de/softwerkskammer/bankocr/AccountNumberParserTest.java
+++ src/test/java/de/softwerkskammer/bankocr/AccountNumberParserTest.java
@@ -18,14 +18,77 @@ public class AccountNumberParserTest {
     @Test
     public void testParseDigit() {
 
-        char[][] testDigit = new char[][] {
+        char[][] testDigit0 = new char[][] {
             {' ','_',' '},
             {'|',' ','|'},
             {'|','_','|'},
         };
 
-        char digit = AccountNumberParser.parseDigit(testDigit);
-        assertEquals("Cannot parse digit", '0', digit);
+        char[][] testDigit1 = new char[][] {
+            {' ',' ',' '},
+            {' ',' ','|'},
+            {' ',' ','|'},
+        };
+
+        char[][] testDigit2 = new char[][] {
+            {' ','_',' '},
+            {' ','_','|'},
+            {' ','|','_'},
+        };
+
+        char[][] testDigit3 = new char[][] {
+            {' ','_',' '},
+            {' ','_','|'},
+            {' ','_','|'},
+        };
+
+        char[][] testDigit4 = new char[][] {
+            {' ',' ',' '},
+            {'|','_','|'},
+            {' ',' ','|'},
+        };
+
+        char[][] testDigit5 = new char[][] {
+            {' ','_',' '},
+            {'|','_',' '},
+            {' ','_','|'},
+        };
+
+        char[][] testDigit6 = new char[][] {
+            {' ','_',' '},
+            {'|','_',' '},
+            {'|','_','|'},
+        };
+
+        char[][] testDigit7 = new char[][] {
+            {' ','_',' '},
+            {' ',' ','|'},
+            {' ',' ','|'},
+        };
+
+        char[][] testDigit8 = new char[][] {
+            {' ','_',' '},
+            {'|','_','|'},
+            {'|','_','|'},
+        };
+
+        char[][] testDigit9 = new char[][] {
+            {' ','_',' '},
+            {'|','_','|'},
+            {' ','_','|'},
+        };
+
+
+        assertEquals("Cannot parse digit", '0', AccountNumberParser.parseDigit(testDigit0));
+        assertEquals("Cannot parse digit", '1', AccountNumberParser.parseDigit(testDigit1));
+        assertEquals("Cannot parse digit", '2', AccountNumberParser.parseDigit(testDigit2));
+        assertEquals("Cannot parse digit", '3', AccountNumberParser.parseDigit(testDigit3));
+        assertEquals("Cannot parse digit", '4', AccountNumberParser.parseDigit(testDigit4));
+        assertEquals("Cannot parse digit", '5', AccountNumberParser.parseDigit(testDigit5));
+        assertEquals("Cannot parse digit", '6', AccountNumberParser.parseDigit(testDigit6));
+        assertEquals("Cannot parse digit", '7', AccountNumberParser.parseDigit(testDigit7));
+        assertEquals("Cannot parse digit", '8', AccountNumberParser.parseDigit(testDigit8));
+        assertEquals("Cannot parse digit", '9', AccountNumberParser.parseDigit(testDigit9));
     }
 
     @Test(expected = IllegalArgumentException.class)

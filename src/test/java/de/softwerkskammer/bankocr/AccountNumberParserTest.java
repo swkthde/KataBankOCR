1ac06f0b41537dc0847787977b9cfd1f476e3578
Jan Mewes <jan.mewes@experimental-software.com>
2016-05-19 08:19:16 +0200

    [Refactor] Clean up

--- src/test/java/de/softwerkskammer/bankocr/AccountNumberParserTest.java
+++ src/test/java/de/softwerkskammer/bankocr/AccountNumberParserTest.java
@@ -2,6 +2,9 @@ package de.softwerkskammer.bankocr;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 
@@ -17,6 +20,22 @@ public class AccountNumberParserTest {
 
     @Test
     public void testParseDigit() {
+
+        List<char[][]> testDigits = getTestDigits();
+
+        assertEquals("Cannot parse digit", '0', AccountNumberParser.parseDigit(testDigits.get(0)));
+        assertEquals("Cannot parse digit", '1', AccountNumberParser.parseDigit(testDigits.get(1)));
+        assertEquals("Cannot parse digit", '2', AccountNumberParser.parseDigit(testDigits.get(2)));
+        assertEquals("Cannot parse digit", '3', AccountNumberParser.parseDigit(testDigits.get(3)));
+        assertEquals("Cannot parse digit", '4', AccountNumberParser.parseDigit(testDigits.get(4)));
+        assertEquals("Cannot parse digit", '5', AccountNumberParser.parseDigit(testDigits.get(5)));
+        assertEquals("Cannot parse digit", '6', AccountNumberParser.parseDigit(testDigits.get(6)));
+        assertEquals("Cannot parse digit", '7', AccountNumberParser.parseDigit(testDigits.get(7)));
+        assertEquals("Cannot parse digit", '8', AccountNumberParser.parseDigit(testDigits.get(8)));
+        assertEquals("Cannot parse digit", '9', AccountNumberParser.parseDigit(testDigits.get(9)));
+    }
+
+    private List<char[][]> getTestDigits() {
 
         char[][] testDigit0 = new char[][] {
             {' ','_',' '},
@@ -78,17 +97,19 @@ public class AccountNumberParserTest {
             {' ','_','|'},
         };
 
+        List<char[][]> testDigits = new ArrayList<>();
+        testDigits.add(testDigit0);
+        testDigits.add(testDigit1);
+        testDigits.add(testDigit2);
+        testDigits.add(testDigit3);
+        testDigits.add(testDigit4);
+        testDigits.add(testDigit5);
+        testDigits.add(testDigit6);
+        testDigits.add(testDigit7);
+        testDigits.add(testDigit8);
+        testDigits.add(testDigit9);
 
-        assertEquals("Cannot parse digit", '0', AccountNumberParser.parseDigit(testDigit0));
-        assertEquals("Cannot parse digit", '1', AccountNumberParser.parseDigit(testDigit1));
-        assertEquals("Cannot parse digit", '2', AccountNumberParser.parseDigit(testDigit2));
-        assertEquals("Cannot parse digit", '3', AccountNumberParser.parseDigit(testDigit3));
-        assertEquals("Cannot parse digit", '4', AccountNumberParser.parseDigit(testDigit4));
-        assertEquals("Cannot parse digit", '5', AccountNumberParser.parseDigit(testDigit5));
-        assertEquals("Cannot parse digit", '6', AccountNumberParser.parseDigit(testDigit6));
-        assertEquals("Cannot parse digit", '7', AccountNumberParser.parseDigit(testDigit7));
-        assertEquals("Cannot parse digit", '8', AccountNumberParser.parseDigit(testDigit8));
-        assertEquals("Cannot parse digit", '9', AccountNumberParser.parseDigit(testDigit9));
+        return testDigits;
     }
 
     @Test(expected = IllegalArgumentException.class)

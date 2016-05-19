91d64923df115b2bb17abdecaf112d98056a1c9c
Jan Mewes <jan.mewes@experimental-software.com>
2016-05-19 08:09:25 +0200

    [Green] Make the test pass

--- src/main/java/de/softwerkskammer/bankocr/AccountNumberParser.java
+++ src/main/java/de/softwerkskammer/bankocr/AccountNumberParser.java
@@ -8,6 +8,24 @@ public class AccountNumberParser {
 
         if (Arrays.deepEquals(digit, Digits.ZERO)) {
             return '0';
+        } else if (Arrays.deepEquals(digit, Digits.ONE)) {
+            return '1';
+        } else if (Arrays.deepEquals(digit, Digits.TWO)) {
+            return '2';
+        } else if (Arrays.deepEquals(digit, Digits.THREE)) {
+            return '3';
+        } else if (Arrays.deepEquals(digit, Digits.FOUR)) {
+            return '4';
+        } else if (Arrays.deepEquals(digit, Digits.FIVE)) {
+            return '5';
+        } else if (Arrays.deepEquals(digit, Digits.SIX)) {
+            return '6';
+        } else if (Arrays.deepEquals(digit, Digits.SEVEN)) {
+            return '7';
+        } else if (Arrays.deepEquals(digit, Digits.EIGHT)) {
+            return '8';
+        } else if (Arrays.deepEquals(digit, Digits.NINE)) {
+            return '9';
         } else {
             throw new IllegalArgumentException("Cannot parse digit " + Arrays.deepToString(digit));
         }
@@ -19,6 +37,60 @@ public class AccountNumberParser {
                 {' ','_',' '},
                 {'|',' ','|'},
                 {'|','_','|'}
+        };
+
+        final static char[][] ONE = new char[][] {
+            {' ',' ',' '},
+            {' ',' ','|'},
+            {' ',' ','|'},
+        };
+
+        final static char[][] TWO = new char[][] {
+            {' ','_',' '},
+            {' ','_','|'},
+            {' ','|','_'},
+        };
+
+        final static char[][] THREE = new char[][] {
+            {' ','_',' '},
+            {' ','_','|'},
+            {' ','_','|'},
+        };
+
+        final static char[][] FOUR = new char[][] {
+            {' ',' ',' '},
+            {'|','_','|'},
+            {' ',' ','|'},
+        };
+
+        final static char[][] FIVE = new char[][] {
+            {' ','_',' '},
+            {'|','_',' '},
+            {' ','_','|'},
+        };
+
+        final static char[][] SIX = new char[][] {
+            {' ','_',' '},
+            {'|','_',' '},
+            {'|','_','|'},
+        };
+
+        final static char[][] SEVEN = new char[][] {
+            {' ','_',' '},
+            {' ',' ','|'},
+            {' ',' ','|'},
+        };
+
+        final static char[][] EIGHT = new char[][] {
+            {' ','_',' '},
+            {'|','_','|'},
+            {'|','_','|'},
+        };
+
+        final static char[][] NINE = new char[][] {
+            {' ','_',' '},
+            {'|','_','|'},
+            {' ','_','|'},
         };
     }
 }
